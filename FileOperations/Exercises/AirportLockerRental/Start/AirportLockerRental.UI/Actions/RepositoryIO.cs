a94b7d610b141fde549691ad2d3daac5855a5b15
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-10 10:58:08 +0200

    update: Airport Locker Renter with a JSON serializer

---
+++ FileOperations/Exercises/AirportLockerRental/Start/AirportLockerRental.UI/Actions/RepositoryIO.cs
@@ -0,0 +1,46 @@
+﻿using AirportLockerRental.UI.DTOs;
+using System.Text.Json;
+
+namespace AirportLockerRental.UI.Actions
+{
+    public class RepositoryIO
+    {
+        private const string _filePath = "lockers.json";
+        private static JsonSerializerOptions _options = new JsonSerializerOptions { WriteIndented = true }; // what does this do?
+
+        public RepositoryIO()
+        {
+            if (!File.Exists(_filePath))
+            {
+                File.WriteAllText(_filePath, "{}");
+            }
+        }
+
+        public Dictionary<int, LockerContents> ReadDataFromFile()
+        {
+            string fileContent = File.ReadAllText(_filePath);
+
+            return JsonSerializer.Deserialize<Dictionary<int, LockerContents>>(fileContent) ?? new Dictionary<int, LockerContents>();
+        }
+
+        public void AddContentsToFile(LockerContents contents)
+        {
+            var repos = ReadDataFromFile();
+            repos.Add(contents.LockerNumber, contents);
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(repos, _options)); // what does this do? the second param specifies how the first param is supposed to be written?
+        }
+
+        public bool RemoveContentsFromFile(LockerContents ContentsToRemove)
+        {
+            var repos = ReadDataFromFile();
+            if (repos.ContainsKey(ContentsToRemove.LockerNumber))
+            {
+                repos.Remove(ContentsToRemove.LockerNumber);
+                File.WriteAllText(_filePath, JsonSerializer.Serialize(repos, _options));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ FileOperations/Exercises/AirportLockerRental/Start/AirportLockerRental.UI/Storage/JsonLockerRepository.cs
@@ -0,0 +1,68 @@
+﻿using AirportLockerRental.UI.Actions;
+using AirportLockerRental.UI.DTOs;
+
+namespace AirportLockerRental.UI.Storage
+{
+    public class JsonLockerRepository : ILockerRepository
+    {
+        private Dictionary<int, LockerContents> _storage;
+        private RepositoryIO _storageIO;
+        public int Capacity { get; set; }
+
+        public JsonLockerRepository(int capacity)
+        {
+            Capacity = capacity;
+            _storage = new Dictionary<int, LockerContents>();
+            _storageIO = new RepositoryIO();
+        }
+
+        public LockerContents? Remove(int number)
+        {
+            if (_storage.ContainsKey(number))
+            {
+                var item = _storage[number];
+                _storage.Remove(number);
+                _storageIO.RemoveContentsFromFile(item);
+                return item;
+            }
+
+            return null;
+        }
+
+        public LockerContents? Get(int number)
+        {
+            if (_storage.ContainsKey(number))
+            {
+                return _storage[number];
+            }
+
+            return null;
+        }
+
+        public bool IsAvailable(int number)
+        {
+            return !_storage.ContainsKey(number);
+        }
+
+        public void List()
+        {
+            foreach (var key in _storage.Keys)
+            {
+                ConsoleIO.DisplayLockerContents(_storage[key]);
+            }
+        }
+
+        public bool Add(LockerContents contents)
+        {
+            if (_storage.ContainsKey(contents.LockerNumber) || _storage.Count >= Capacity)
+            {
+                return false;
+            }
+
+            _storage.Add(contents.LockerNumber, contents);
+            _storageIO.AddContentsToFile(contents);
+
+            return true;
+        }
+    }
+}
