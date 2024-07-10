a3b71fb4c6a97f57c0388f1cd6ed37fb2b6a4e42
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-10 08:39:37 +0200

    update: SystemIO experiements

--- FileOperations/Examples/SystemIOExamples/SystemIOExamples/Program.cs
+++ FileOperations/Examples/SystemIOExamples/SystemIOExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SystemIOExamples;
+using static System.Net.Mime.MediaTypeNames;
 
 //string path1 = @"D:\GitHub\CloneRepository\C#300\text1.txt";
 //PathExamples.PrintFileName(path1);
@@ -17,18 +18,38 @@
 //}
 
 // Create new folders
-Directory.CreateDirectory(@"D:\testFolder1");
-Directory.CreateDirectory(@"D:\testFolder1\nestedFolder1");
+//Directory.CreateDirectory(@"D:\testFolder1");
+//Directory.CreateDirectory(@"D:\testFolder1\nestedFolder1");
 
 // Use File.WriteAllLines to create a file in a folder 
-string[] lines = { "This is a test file.", "It has several lines of texts.", "This is another line.", "This is the end of the text." };
-File.WriteAllLines(@"D:\testFolder1\nestedFolder1\testFile.txt", lines);
+//string[] lines = { "This is a test file.", "It has several lines of texts.", "This is another line.", "This is the end of the text." };
+//string text = "Adding some text: We are thrilled to announce that you are the perfect candidate for this job.\nWe look forward to meeting you soon.";
+//File.WriteAllLines(@"D:\testFolder1\nestedFolder1\testFile.txt", lines);
+//File.AppendAllText(@"D:\testFolder1\nestedFolder1\testFile.txt", text);
+//File.WriteAllText(@"D:\testFolder1\nestedFolder1\testFile.txt", text);
 
+// Copy files
+//File.Copy(@"D:\testFolder1\nestedFolder1\testFile.txt", @"D:\testFolder1\nestedFolder1\testFile2.txt");
+
+// Rename files
+//File.Move(@"D:\testFolder1\nestedFolder1\testFile.txt", @"D:\testFolder1\nestedFolder1\renamedTextFile.txt");
+
+// Delete Files
+/*Directory.Delete(@"D:\testFolder1");*/ // this will throw an error: directory not empty
+//Directory.Delete(@"D:\testFolder1", true); // this will override the error.
+//if (Directory.Exists(@"D:\testFolder1"))
+//{
+//    Directory.Delete(@"D:\testFolder1", true);
+//}
+//else
+//{
+//    Directory.Delete(@"D:\testFolder1);
+//}
 //DirectoryExamples.PrintCurrentDirectory();
 //DirectoryExamples.PrintLogicalDrives();
 //DirectoryExamples.PrintCurrentDirectoryFiles();
 //DirectoryExamples.PrintParentDirectory();
-DirectoryExamples.PrintDirectories(@"D:\GitHub\CloneRepository");
+//DirectoryExamples.PrintDirectories(@"D:\GitHub\CloneRepository");
 
 //DirectoryInfoExamples.GetFileInfoList();
 //DirectoryInfoExamples.GetFilePaths();
