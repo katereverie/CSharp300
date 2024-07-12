2dc961b12ab91cb5b07310ebe138691903b51a73
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-12 15:54:08 +0200

    update: CustomWord implementation

---
+++ FileOperations/Exercises/Hangman/Hangman/Hangman.UI/WordSources/CustomWord.cs
@@ -0,0 +1,38 @@
+﻿using Hangman.UI.Interfaces;
+
+namespace Hangman.UI.WordSources
+{
+    public class CustomWord : IWordSource
+    {
+        /// <summary>
+        /// evaluates string input against being empty, or numeric, or consisting of multiple words.
+        /// </summary>
+        /// <returns>a single non-numeric, non-empty, word</returns>
+        public string GetWord()
+        {
+            while (true)
+            {
+                Console.Write("Enter Word: ");
+                string? word = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(word))
+                {
+                    Console.WriteLine("Your word mustn't be empty.");
+                    continue;
+                }
+                else if (int.TryParse(word, out int nonword))
+                {
+                    Console.WriteLine("Your word musn't be numeric.");
+                    continue;
+                }
+                else if (word.Trim().Split(' ').Length != 1)
+                {
+                    Console.WriteLine("Your word mustn't contain space in between.");
+                    continue;
+                }
+
+                return word;
+            }
+        }
+    }
+}
