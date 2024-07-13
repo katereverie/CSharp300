284254169ba2259248e920e36e83f7cbf71cb5b2
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-13 13:13:45 +0200

    update: state manager

--- FileOperations/Exercises/Hangman/Hangman/Hangman.BLL/GameManager.cs
+++ FileOperations/Exercises/Hangman/Hangman/Hangman.BLL/GameManager.cs
@@ -7,6 +7,7 @@
             return guessRecord.Contains(guess);
         }
 
+        // word and guess are guaranteed to be clean: all lowercase and etc. because they have been handled by IO-related methods.
         public MatchResult CheckGuessMatch(string word, string guess)
         {
             switch (guess.Length)
@@ -16,6 +17,16 @@
                 default:
                     return word == guess ? MatchResult.FullMatch : MatchResult.NoMatch;
             }
+        }
+
+        public bool CheckGuesserWin(int matchCount, int wordLength, MatchResult? result)
+        {
+            return matchCount == wordLength || result == MatchResult.FullMatch;
+        }
+
+        public bool CheckGuesserLose(int strikesLeft)
+        {
+            return strikesLeft == 0;
         }
     }
 }
