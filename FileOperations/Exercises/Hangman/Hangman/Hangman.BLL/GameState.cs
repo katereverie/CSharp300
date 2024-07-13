9c0f7877d9cded2aa759909bc5e70354360a96fd
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-13 13:14:23 +0200

    update: move Word, Matches to Workflow

--- FileOperations/Exercises/Hangman/Hangman/Hangman.BLL/GameState.cs
+++ FileOperations/Exercises/Hangman/Hangman/Hangman.BLL/GameState.cs
@@ -2,28 +2,23 @@
 {
     public class GameState
     {
-        public List<string> GuessRecord { get; set; }
+        public List<string> GuessRecord { get; set; } = new List<string>();
         public Dictionary<string, int> PlayerScores { get; set; }
-        public string? Word{ get; set; }
-        public int StrikesLeft { get; set; }
+        public int StrikesLeft { get; set; } = 5;
 
         public GameState(string p1Name, string p2Name)
         {
-            GuessRecord = new List<string>();
-            // what if players name are duplicates?
             PlayerScores = new Dictionary<string, int>
             {
                 {p1Name, 0},
                 {p2Name, 0}
             };
-            StrikesLeft = 5;
         }
 
         public void Reset()
         {
             GuessRecord = new List<string>();
             StrikesLeft = 5;
-            Word = null;
         }
     }
 }
