fa30727d651f6df1dac4e20dacb702e7358cc4f9
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-19 16:52:04 +0200

    update: add dependencies

--- FileOperations/Examples/ConfigFiles/ConfigFiles/ConfigFiles/Program.cs
+++ FileOperations/Examples/ConfigFiles/ConfigFiles/ConfigFiles/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+// creating an instance of the config builder and loading the appsettings.json file
+var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+// now we can get a configuration section by name
+var configSection = configBuilder.GetSection("AppSettings");
+
+// get the config values in the section
+var apiKey = configSection["API_Key"] ?? null;
+var logDirectory = configSection["LogDir"] ?? null;
+var db = configSection["ConnectionStrings:4WS"] ?? null;
+
+Console.WriteLine($"key:{apiKey}\nlogdir:{logDirectory}\ndb:{db}\n");
