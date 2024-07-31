7950b2bf60960efee2b2b10d2c8c344df5745238
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-07-31 17:12:15 +0200

    fix: improper mapping

--- NTierArchitecture/LibraryManagement/LibraryManagement.Core/Entities/MediaCheckoutCount.cs
+++ NTierArchitecture/LibraryManagement/LibraryManagement.Core/Entities/MediaCheckoutCount.cs
@@ -4,7 +4,7 @@
     {
         public int MediaID { get; set; }
         public string MediaTypeName { get; set; }
-        public string MediaTitle { get; set; }
+        public string Title { get; set; }
         public int CheckoutCount { get; set; }
     }
 }
