c39e5511911bf09ff47ff7bf20b564f3c0736151
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-02 18:08:16 +0200

    update: a MediaType can have no corresponding media  in the librarry

--- NTierArchitecture/LibraryManagement/LibraryManagement.Core/Entities/MediaType.cs
+++ NTierArchitecture/LibraryManagement/LibraryManagement.Core/Entities/MediaType.cs
@@ -5,6 +5,6 @@
         public int MediaTypeID { get; set; }
         public string MediaTypeName { get; set; }
 
-        public List<Media> Medias { get; set; }
+        public List<Media>? Medias { get; set; }
     }
 }
