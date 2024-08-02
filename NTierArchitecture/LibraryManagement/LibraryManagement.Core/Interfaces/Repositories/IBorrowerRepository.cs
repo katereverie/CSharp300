0010984622593b7813c93e66feb53e4d54abc4fb
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-02 18:09:03 +0200

    update: modify Delete and Update to return bool

--- NTierArchitecture/LibraryManagement/LibraryManagement.Core/Interfaces/Repositories/IBorrowerRepository.cs
+++ NTierArchitecture/LibraryManagement/LibraryManagement.Core/Interfaces/Repositories/IBorrowerRepository.cs
@@ -5,10 +5,10 @@ namespace LibraryManagement.Core.Interfaces.Repositories
     public interface IBorrowerRepository
     {
         int Add(Borrower newBorrower);
-        void Delete(Borrower borrower);
-        void Update(Borrower request);
+        bool Delete(Borrower borrower);
+        bool Update(Borrower request);
         List<Borrower> GetAll(); 
-        List<CheckoutLog> GetCheckoutLogs(Borrower borrower);
+        List<CheckoutLog>? GetCheckoutLogs(Borrower borrower);
         Borrower? GetByEmail(string email);
     }
 }
