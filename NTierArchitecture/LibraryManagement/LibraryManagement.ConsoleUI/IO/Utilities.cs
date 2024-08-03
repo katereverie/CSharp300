cb474ac06a1efa163181d988e08a2490b0ae0811
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-03 13:51:34 +0200

    Update: Add PrintBorrowerInformation, PrintBorrowerCheckoutLog

--- NTierArchitecture/LibraryManagement/LibraryManagement.ConsoleUI/IO/Utilities.cs
+++ NTierArchitecture/LibraryManagement/LibraryManagement.ConsoleUI/IO/Utilities.cs
@@ -1,4 +1,6 @@
-﻿namespace LibraryManagement.ConsoleUI.IO
+﻿using LibraryManagement.Core.Entities;
+
+namespace LibraryManagement.ConsoleUI.IO
 {
     public class Utilities
     {
@@ -46,5 +48,40 @@
                 AnyKey();
             } while (true);
         }
+
+        public static void PrintBorrowerList(List<Borrower> list, string header = " Borrower List ")
+        {
+            string headerSpace = new string('=', (100 - header.Length) / 2);
+            Console.WriteLine("\n" + headerSpace + header + headerSpace);
+            Console.WriteLine($"{"ID",-5} {"Name",-32} Email");
+            Console.WriteLine(new string('=', 70));
+            foreach (var b in list)
+            {
+                Console.WriteLine($"{b.BorrowerID,-5} {b.LastName + ", " + b.FirstName,-32} {b.Email}");
+            }
+        }
+
+        public static void PrintBorrowerInformation(Borrower borrower, string header = " Borrower Information ")
+        {
+            string headerSpace = new string('=', (100 - header.Length) / 2);
+            Console.WriteLine("\n" + headerSpace + header + headerSpace);
+            Console.WriteLine($"Id: {borrower.BorrowerID}");
+            Console.WriteLine($"Name: {borrower.LastName}, {borrower.FirstName}");
+            Console.WriteLine($"Email: {borrower.Email}");
+        }
+
+        public static void PrintBorrowerCheckoutLog(List<CheckoutLog> logs, string header = " Checkout Record ")
+        {
+            string headerSpace = new string('=', (100 - header.Length) / 2);
+            Console.WriteLine("\n" + headerSpace + header + headerSpace);
+            Console.WriteLine($"{"Media ID",-10} {"Title",-40} {"Checkout Date",-20} {"Return Date",-20}");
+            foreach (var cl in logs)
+            {
+                Console.WriteLine($"{cl.MediaID,-10} " +
+                    $"{cl.Media.Title,-40} " +
+                    $"{cl.CheckoutDate,-20:MM/dd/yyyy} " +
+                    $"{(cl.ReturnDate == null ? "Unreturned" : cl.ReturnDate),-20:MM/dd/yyyy}");
+            }
+        } 
     }
 }
