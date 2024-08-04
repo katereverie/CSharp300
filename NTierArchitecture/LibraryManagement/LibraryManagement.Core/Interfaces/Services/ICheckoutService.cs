6df217b53f84726b3d0b3f8d5a13b6f9e1a9148a
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-04 12:35:33 +0200

    update: rename GetAllUncheckedoutUnarchivedMedia to GetAvailableMedia update: modify GetCheckedoutMediaByBorrowerID to return CheckoutLog

--- NTierArchitecture/LibraryManagement/LibraryManagement.Core/Interfaces/Services/ICheckoutService.cs
+++ NTierArchitecture/LibraryManagement/LibraryManagement.Core/Interfaces/Services/ICheckoutService.cs
@@ -5,9 +5,9 @@ namespace LibraryManagement.Core.Interfaces.Services
     public interface ICheckoutService
     {
         Result<List<CheckoutLog>> GetAllCheckedoutMedia();
-        Result<List<Media>> GetAllUncheckedoutUnarchivedMedia();
+        Result<List<Media>> GetAvailableMedia();
         Result<List<CheckoutLog>> GetCheckoutLogsByBorrowerID(int borrowerID);
-        Result<List<CheckoutLogDto>> GetCheckedOutMediaByBorrowerID(int borrowerID);
+        Result<List<CheckoutLog>> GetCheckedOutMediaByBorrowerID(int borrowerID);
         Result<int> CheckoutMedia(CheckoutLog newCheckoutLog); 
         Result<Borrower> GetBorrowerByEmail(string email);
         Result CheckBorrowStatus(int borrowerID);
